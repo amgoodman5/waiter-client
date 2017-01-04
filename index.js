162cb2da068e043f0e6ec8f644cf9a99d454d50b
Aaron Goodman <Amgoodman5@Aarons-MacBook-Air.local>
2017-01-04 09:50:08 -0700

    minorstyles

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-
+// jquery style stuff
+/*carousel*/
 $( document ).ready(function() {
 $('.carousel').carousel()
+
+
+
+
+
+/*appearence*/
+    $(window).scroll( function(){
+
+        $('.row').each( function(i){
+
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
+            bottom_of_window = bottom_of_window + 300;
+
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},500);
+
+
+            }
+        });
+
+    });
+
 });
