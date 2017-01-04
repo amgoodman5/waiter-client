c9b6dc3e8e36f5fd7b15d516bb3be349aea9b205
Dillon Corkran <dcorkran972@gmail.com>
2017-01-03 20:00:31 -0700

    :sparkles: Added post user route functionality

---
+++ assets/scripts/signup.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+  $( "#signup-form" ).on( "submit", function( event ) {
+    event.preventDefault();
+    var userData = $( this ).serialize()
+    postNewUser(userData);
+  });
+});
+
+function postNewUser(formData){
+  //error keeps triggering but it posts to db... is it because it is asynchronous?
+  var request = $.ajax({
+    url: 'https://line-waiter-db.herokuapp.com/users',
+    method: 'POST',
+    data: formData,
+    dataType: 'application/json',
+    success:function(msg){
+      console.log(msg);
+      console.log('done!');
+    },
+    error:function(msg){
+      console.log('error!');
+      console.log(msg);
+    }
+  });
+}
