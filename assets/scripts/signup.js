79e9c9da1f1c75abc4cd6d0685965777a7325ca3
Murph Grainger <murph.grainger@gmail.com>
2017-01-05 15:49:53 -0700

    linked firebase routes for heroku

--- assets/scripts/signup.js
+++ assets/scripts/signup.js
@@ -1,34 +1,35 @@
 const SERVER_URL = getUrl1();
 const CLIENT_URL = getUrl2();
 
-$( document ).ready(function() {
-  $( "#signup-form" ).on( "submit", function( event ) {
-    event.preventDefault();
-    var userData = $( this ).serialize();
-    postNewUser(userData)
-    .then((data)=>{
-      console.log(data);
-      window.location.replace(`${CLIENT_URL}/dashboard.html`);
+$(document).ready(function() {
+    $("#signup-form").on("submit", function(event) {
+        event.preventDefault();
+        var userData = $(this).serialize();
+        postNewUser(userData)
+            .then((data) => {
+                console.log(data);
+                window.location.replace(`${CLIENT_URL}/dashboard.html`);
+            });
     });
-  });
 });
 
-function postNewUser(formData){
-  console.log('try to post');
-  return $.post(`${SERVER_URL}/authAPI/new`,formData);
+function postNewUser(formData) {
+    console.log('try to post');
+    return $.post(`${SERVER_URL}/authAPI/new`, formData);
 }
 
-function getUrl1(){
-  if (window.location.host.indexOf('localhost') != -1) {
-    return 'http://localhost:3000';
-  } else {
-    return 'https://line-waiter-db.herokuapp.com';
-  }
+function getUrl1() {
+    if (window.location.host.indexOf('localhost') != -1) {
+        return 'http://localhost:3000';
+    } else {
+        return 'https://line-waiter-db.herokuapp.com';
+    }
 };
-function getUrl2(){
-  if (window.location.host.indexOf('localhost') != -1) {
-    return 'http://localhost:8080';
-  } else {
-    return 'https://line-waiter.firebaseapp.com/';
-  }
+
+function getUrl2() {
+    if (window.location.host.indexOf('localhost') != -1) {
+        return 'http://localhost:8080';
+    } else {
+        return 'https://line-waiter.firebaseapp.com';
+    }
 };
