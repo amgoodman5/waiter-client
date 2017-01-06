58725934eb2b7c3532e8bf563c6a3306ab041e8c
Aaron Goodman <Amgoodman5@Aarons-MacBook-Air.local>
2017-01-06 11:46:56 -0700

    billboard updates

--- assets/scripts/billboard.js
+++ assets/scripts/billboard.js
@@ -12,13 +12,19 @@ $(document).ready(() => {
         .then((jobs) => {
             console.log(jobs);
             jobs.forEach((job) => {
-                var card = `<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-      <div class="panel panel-default"> <div class="panel-heading" role="tab" id="headingOne">
-       <h4 class="panel-title"> <a role="button" data-toggle="collapsed" data-parent="#accordion" href="${job.id}" aria-expanded="true" aria-controls="collapseOne">
-       <h4>Location: ${job.name}</a></h4></div>
-      <div id="${job.id}" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="${job.id}">
-      <div class="panel-body">  <h4>Address: ${job.address}</h4><h4>Starts: ${job.start_time}</h4>
-      <h4> <button data-id="${job.id}"class="btn btn-warning card-link accept-button" type="submit">Accept Job</button></div> <div class="panel-body">  </div>  </div></div>`;
+
+                var card = `
+
+<div id="accordion" role="tablist" aria-multiselectable="true">
+    <div class="card">  <div class="row" role="tab" id="card-header">
+    <div class=" job-headers"><h2>
+    <a class="collapsed" data-toggle="collapse" data-target="#${job.id}" aria-expanded="false" aria-controls="collapseExample">
+  <h4>Location: ${job.name}</h4></a><h4>Address: ${job.address}</h4><h4>Starts: ${job.start_time}</h4></p>
+  <div class="collapse" id="${job.id}"><div class="card card-block">
+<div class="panel-body">
+ <button data-id="${job.id}"class="btn btn-warning card-link accept-button" type="submit">Accept Job</button>
+ </div> <div class="panel-body"></div></div></div></div></div></div></div>`
+
                 $(".job_cards").append(card)
             });
             $('.accept-button').on('click', function(event) {
